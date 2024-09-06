0acf90df5a9d9bbf35da250a753ebf4f93c2fea5
Divyanshu Raj <divyanshuraj23102005@gmail.com>
2024-09-06 20:33:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const navbar = document.getElementById('navbar');
+function handleScroll() {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+}
+window.addEventListener('scroll', handleScroll);
